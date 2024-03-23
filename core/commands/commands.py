a100b44e0ec47149b8042b6fad0e18790a26dc05
rilistx <rilistx@gmail.com>
2024-03-23 20:24:25 +0200

    init

---
+++ core/commands/commands.py
@@ -0,0 +1,7 @@
+from aiogram.types import BotCommand
+
+
+commands = [
+    BotCommand(command='start', description='Старт бота'),
+    BotCommand(command='inline', description='Инлайн клавиатура'),
+]
